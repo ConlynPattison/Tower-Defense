45a8881db0546f560d35f1f85759ff7a2c4e4fd6
ConlynPattison <cpattison@csumb.edu>
2023-03-15 01:15:02 -0700

    up to video 3: wavespawner basis; timer countdown ui

---
+++ Assets/WaveSpawner.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+    public TextMeshProUGUI waveCountdownText;
+    public float timeBetweenWaves = 5f;
+
+    private float _countDown = 2f;
+    private int _waveIndex = 0;
+    
+    private void Update()
+    {
+        if (_countDown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            _countDown = timeBetweenWaves;
+        }
+
+        _countDown -= Time.deltaTime;
+
+        waveCountdownText.text = Mathf.Ceil(_countDown).ToString();
+    }
+
+    IEnumerator SpawnWave()
+    {
+        _waveIndex++;
+        
+        for (int i = 0; i < _waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
