8f5db0f91b5af0a6cd563da8b06d1e431cb4c9c5
ConlynPattison <cpattison@csumb.edu>
2023-04-01 16:07:35 -0700

    (Vid 21) Added: Pause menu; Removed: Camera component's function to freeze movement

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private bool _doMovement = true;
-    
     public float panSpeed = 30f;
     public float panBorderThickness = 10f;
 
@@ -21,13 +19,7 @@ public class CameraController : MonoBehaviour
             this.enabled = false;
             return;
         }
-        
-        if (Input.GetKeyDown(KeyCode.Escape))
-            _doMovement = !_doMovement;
-        
-        if (!_doMovement)
-            return;
-        
+
         if (Input.GetKey(KeyCode.W) || Input.mousePosition.y >= Screen.height - panBorderThickness)
         {
             transform.Translate(Time.deltaTime * panSpeed * Vector3.forward, Space.World);
